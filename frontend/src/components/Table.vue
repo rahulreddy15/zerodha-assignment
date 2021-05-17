a6eb53d7c6bbf8e8ceaf7db60e2db419c41413a6
Rahul <rahulanna97@gmail.com>
2021-05-17 14:09:06 +0530

    Fixed Table

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -7,38 +7,63 @@
         :value="products"
         responsiveLayout="scroll"
         :paginator="true"
+        ref="dt"
         removableSort
+        :filters="filters"
+        :loading="loading"
         :rows="10"
         paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
         :rowsPerPageOptions="[10, 20, 50]"
         currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
       >
+        <template #header>
+          <div style="text-align: left">
+            <Button
+              icon="pi pi-external-link"
+              label="Export To CSV"
+              @click="exportCSV($event)"
+            />
+          </div>
+          <div class="text-align: right">
+            <Button
+              type="button"
+              icon="pi pi-filter-slash"
+              label="Clear"
+              class="p-button-outlined"
+              @click="clearFilter()"
+            />
+            <span class="p-input-icon-left">
+              <i class="pi pi-search" />
+              <InputText
+                v-model="filters['global'].value"
+                placeholder="Keyword Search"
+              />
+            </span>
+          </div>
+        </template>
+        <template #loading>
+          Loading customers data. Please wait.
+        </template>
+        <template #empty>
+          No data found.
+        </template>
         <Column field="SC_CODE" header="Code" sortable="true"></Column>
         <Column field="SC_NAME" header="Name" sortable="true"></Column>
-        <Column field="SC_GROUP" header="Group" sortable="true"></Column>
-        <Column field="SC_TYPE" header="Type" sortable="true"></Column>
         <Column field="OPEN" header="Open" sortable="true"></Column>
         <Column field="HIGH" header="High" sortable="true"></Column>
         <Column field="LOW" header="Low" sortable="true"></Column>
         <Column field="CLOSE" header="Close" sortable="true"></Column>
         <Column field="LAST" header="Last" sortable="true"></Column>
-        <Column
-          field="NO_TRADES"
-          header="Number Of Trades"
-          sortable="true"
-        ></Column>
+        <Column field="NO_TRADES" header="Trades" sortable="true"></Column>
+        <Column field="NO_OF_SHRS" header="Shares" sortable="true"></Column>
+        <Column field="NET_TURNOV" header="Turnover" sortable="true"></Column>
       </DataTable>
     </div>
-    <!-- <pre>
-        <code>
-            {{stringify(products)}}
-        </code>
-    </pre> -->
   </div>
 </template>
 
 <script>
-// import ProductService from "./ProductService";
+import { FilterMatchMode } from "primevue/api";
 import axios from "axios";
 export default {
   name: "Table",
@@ -47,6 +72,10 @@ export default {
       products: null,
       date: null,
       count: null,
+      filters: {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      },
+      loading: true,
     };
   },
   productService: null,
@@ -58,10 +87,22 @@ export default {
       this.date = res.data.date;
       this.count = res.data.count;
     });
+    this.loading = false;
   },
   methods: {
     stringify(value) {
       return JSON.stringify(value, null, 2);
+    },
+    exportCSV() {
+      this.$refs.dt.exportCSV();
+    },
+    clearFilter() {
+      this.initFilters();
+    },
+    initFilters() {
+      this.filters = {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      };
     },
   },
 };
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
 
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -13,5 +15,7 @@ const app = createApp(App);
 app.use(PrimeVue);
 app.component("DataTable", DataTable);
 app.component("Column", Column);
+app.component("Button", Button);
+app.component("InputText", InputText);
 
 app.mount("#app");
